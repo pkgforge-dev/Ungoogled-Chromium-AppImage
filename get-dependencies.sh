0dbe646f84d82b90fe76dc3cea7949eb9b69e558
Link4Electronics <link4electronics@gmail.com>
2026-09-11 04:22:39 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,29 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm pipewire-audio pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use ARM64 for the tar links
+	x86_64)  tar_arch=x86_64;;
+	aarch64) tar_arch=arm64;;
+esac
+TAR_LINK=$(wget https://api.github.com/repos/ungoogled-software/ungoogled-chromium-portablelinux/releases -O - \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*${tar_arch}_linux.tar.xz")
+echo "$TAR_LINK" | awk -F'/' '{gsub(/^v/, "", $(NF-1)); print $(NF-1); exit}' > ~/version
+if ! wget --retry-connrefused --tries=30 "$TAR_LINK" -O /tmp/app.tar.xz 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+tar -xvf /tmp/app.tar.xz --strip-components=1 -C ./AppDir/bin
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+# we need to remove this because chrome otherwise dlopen libQt5Core on the host
+# when present, we can only bunle libqt6 or libqt5 but not both
+rm -f ./AppDir/bin/libqt5_shim.so

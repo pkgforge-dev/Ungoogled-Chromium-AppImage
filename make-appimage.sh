9eb4305e6d4abbd8b1d37848850231f4ed6123eb
Link4Electronics <link4electronics@gmail.com>
2026-09-13 16:05:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
+export DEPLOY_P11KIT=1
 export URUNTIME_PRELOAD=1
 
 # Deploy dependencies
